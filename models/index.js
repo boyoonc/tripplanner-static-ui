a789e87cc759480653f364898b77b7098d8c38fb
Jonathan Brandwein <jonathan.brandwein@gmail.com>
2017-08-15 18:52:44 -0400

    seeded db!

--- models/index.js
+++ models/index.js
@@ -1,15 +1,25 @@
 const Sequelize = require('sequelize');
 
-const conn = new Sequelize({url:process.env.DATABASE_URL, dialect: 'postgres'});
+const conn = new Sequelize(process.env.DATABASE_URL, {
+  logging: false
+  });
 
-const Foo = conn.define('foo', {});
+const Foo = conn.define('foo', {
+  name: Sequelize.STRING
+});
 
 const sync = ()=> {
   return conn.sync({ force: true });
 };
 
+const seed = () => {
+  return Foo.create({
+    name: 'Jon'
+  });
+}
 module.exports = {
   sync,
+  seed,
   models: {
     Foo
   }
